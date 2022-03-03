088284912954377fe83e09b4f8900bcff8c91873
NightOwl721 <Hymm121@outlook.com>
2022-03-03 16:09:08 +0100

    Update github.js
    Update command

--- commands/Information/github.js
+++ commands/Information/github.js
@@ -4,6 +4,7 @@ const config = require("../../botconfig/config.json");
 const ee = require("../../botconfig/embed.json");
 const RedditImageFetcher = require("reddit-image-fetcher");
 const fs = require('fs')
+const axios = require('axios')
 module.exports = {
     name: "github",
     category: "Information",
@@ -13,43 +14,29 @@ module.exports = {
     description: "Wysyła informacje o githubie podanego użytkownika",
     run: async (client, message, args, user, text, prefix) => {
         try {
-            if (context.params.event.content.startsWith("!github")) {
-                const search = context.params.event.content.split(" ").slice(1)
-                if (!search[0]) return lib.discord.channels['@0.1.1'].messages.create({
-                    channel_id: context.params.event.channel_id,
-                    content: `Wpisz nazwe uzytkownika.`
-                });
+            if (!args[0]) return message.channel.send("Podaj nazwę użytkownika!")
+            let user = args[0]
+            let url = `https://api.github.com/users/${user}`
+            let { data } = await axios.get(url)
+            let { avatar_url, name, bio, company, location, blog, public_repos, followers, following, created_at } = data
+            let embed = new MessageEmbed()
+                .setColor(ee.color)
+                .setTitle(`Informacje o ${name}`)
 
-                let data = await axios(` https://api.github.com/users/${search[0]}`, {
-                    headers: {
-                        accept: "application/vnd.github.v3+json"
-                    }
-                }).catch(err => { })
+                .setThumbnail(avatar_url)
+                .addField("Nazwa", name, true)
+                .addField("O nim:", bio, true)
+                .addField("Firma", company, true)
+                .addField("Lokalizacja", location, true)
+                .addField("Blog", blog, true)
+                .addField("Repozytoria", public_repos, true)
+                .addField("Obserwujacy", followers, true)
+                .addField("Obserwuje", following, true)
+                .addField("Data utworzenia", created_at, true)
+                .setFooter(`Night Owl ©️ ${new Date().getFullYear()}`)
+                .setTimestamp()
+            message.channel.send(embed)
 
-                if (!data) return lib.discord.channels['@0.1.1'].messages.create({
-                    channel_id: context.params.event.channel_id,
-                    content: `Nie ma takiego użytkownika!`
-                })
-                else data = data.data
-
-                await lib.discord.channels['@0.1.1'].messages.create({
-                    channel_id: context.params.event.channel_id,
-                    content: ``,
-                    embed: {
-                        title: data.name,
-                        url: `https://github.com/${data.login}`,
-                        description: data.bio,
-                        thumbnail: { url: data.avatar_url },
-                        color: 0x161b22,
-                        fields: [
-                            { name: "Obserwujacy: ", value: data.followers, inline: true },
-                            { name: "Publiczne Reprozytoria: ", value: data.public_repos, inline: true },
-                            { name: "Konto utworzone: ", value: `${new Date(data.created_at)}` }
-                        ]
-                    }
-                });
-
-            }
         } catch (e) {
             console.log(String(e.stack).bgRed);
         }
