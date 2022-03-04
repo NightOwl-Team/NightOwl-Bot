5da6d47db06b220d647ddef08201e6ceb9cdc10a
NightOwl721 <Hymm121@outlook.com>
2022-03-04 17:39:55 +0100

    Update uno.js

--- commands/Fun/uno.js
+++ commands/Fun/uno.js
@@ -47,78 +47,80 @@ module.exports = {
                     .setTitle(`❌ ERROR | Ten gracz nie jest na tym serwerze`)
                 );
                 //ask user if he wants to challenge
-                message.channel.send(new MessageEmbed()
-                    .setColor(ee.color)
-                    .setFooter(ee.footertext, ee.footericon)
-                    .setTitle(`❓ Czy chesz zakceptowac wyzwanie?`)
-                    .setDescription(`${user}`)
-                    .addField(`${message.author} zadaje wyzwanie do gry`)
-                ).then(async msg => {
-                    await msg.react("✅");
-                    await msg.react("❌");
-                    const filter = (reaction, user) => reaction.emoji.name === "✅" && user.id === user.id;
-                    const collector = msg.createReactionCollector(filter, { time: 6000 });
-                    collector.on("collect", async r => {
-                        if (r.emoji.name === "✅") {
-                            let i = 0;
-                            msg.delete();
-                            message.channel.send(new MessageEmbed()
-                                .setColor(ee.color)
-                                .setFooter(ee.footertext, ee.footericon)
-                                .setTitle(`✅ Wyzwanie zostalo zaakceptowane`)
-                                .setDescription(`Trwa przygotowywanie gry {$i}`)
-                            ).then(async msg => {
+                if (args[0] === "challenge") {
+                    message.channel.send(new MessageEmbed()
+                        .setColor(ee.color)
+                        .setFooter(ee.footertext, ee.footericon)
+                        .setTitle(`❓ Czy chesz zakceptowac wyzwanie?`)
+                        .setDescription(`${user}`)
+                        .addField(`${message.author} zadaje wyzwanie do gry`)
+                    ).then(async msg => {
+                        await msg.react("✅");
+                        await msg.react("❌");
+                        const filter = (reaction, user) => reaction.emoji.name === "✅" && user.id === user.id;
+                        const collector = msg.createReactionCollector(filter, { time: 6000 });
+                        collector.on("collect", async r => {
+                            if (r.emoji.name === "✅") {
+                                let i = 0;
+                                msg.delete();
+                                message.channel.send(new MessageEmbed()
+                                    .setColor(ee.color)
+                                    .setFooter(ee.footertext, ee.footericon)
+                                    .setTitle(`✅ Wyzwanie zostalo zaakceptowane`)
+                                    .setDescription(`Trwa przygotowywanie gry {$i}`)
+                                ).then(async msg => {
 
-                                //edit message every second adding a percentege
-
-                                let interval = setInterval(async () => {
-                                    i++;
                                     //edit message every second adding a percentege
-                                    msg.edit(new MessageEmbed()
-                                        .setColor(ee.color)
-                                        .setFooter(ee.footertext, ee.footericon)
-                                        .setTitle(`✅ Wyzwanie zostalo zaakceptowane`)
-                                        .setDescription(`Trwa przygotowywanie gry {$i}`)
-                                    );
-                                    if (i === 100) {
-                                        clearInterval(interval);
-                                        msg.delete();
-                                        message.channel.send(new MessageEmbed()
+
+                                    let interval = setInterval(async () => {
+                                        i++;
+                                        //edit message every second adding a percentege
+                                        msg.edit(new MessageEmbed()
                                             .setColor(ee.color)
                                             .setFooter(ee.footertext, ee.footericon)
                                             .setTitle(`✅ Wyzwanie zostalo zaakceptowane`)
-                                            .setDescription(`Gra zostala przygotowana- Zaczynamy!`)
-                                        ).then(async msg => {
-                                            msg.delete({ timeout: 5000 })
-                                            //start game
+                                            .setDescription(`Trwa przygotowywanie gry {$i}`)
+                                        );
+                                        if (i === 100) {
+                                            clearInterval(interval);
+                                            msg.delete();
                                             message.channel.send(new MessageEmbed()
                                                 .setColor(ee.color)
                                                 .setFooter(ee.footertext, ee.footericon)
-                                                .setTitle(`Rozdaje karty`)
-                                                .setDescription(`Chwileczkę...`)
+                                                .setTitle(`✅ Wyzwanie zostalo zaakceptowane`)
+                                                .setDescription(`Gra zostala przygotowana- Zaczynamy!`)
                                             ).then(async msg => {
                                                 msg.delete({ timeout: 5000 })
-                                            })
+                                                //start game
+                                                message.channel.send(new MessageEmbed()
+                                                    .setColor(ee.color)
+                                                    .setFooter(ee.footertext, ee.footericon)
+                                                    .setTitle(`Rozdaje karty`)
+                                                    .setDescription(`Chwileczkę...`)
+                                                ).then(async msg => {
+                                                    msg.delete({ timeout: 5000 })
+                                                })
 
-                                        });
+                                            });
 
-                                    }
-                                }, 1000);
-                            });
-                        }
+                                        }
+                                    }, 1000);
+                                });
+                            }
+                        });
+                        //exit if user clicks on decline
+                        collector.on("end", collected => {
+                            if (collected.size === 0) {
+                                msg.delete();
+                                message.channel.send(new MessageEmbed()
+                                    .setColor(ee.color)
+                                    .setFooter(ee.footertext, ee.footericon)
+                                    .setTitle(`❌ Wyzwanie zostalo odrzucone`)
+                                );
+                            }
+                        });
                     });
-                    //exit if user clicks on decline
-                    collector.on("end", collected => {
-                        if (collected.size === 0) {
-                            msg.delete();
-                            message.channel.send(new MessageEmbed()
-                                .setColor(ee.color)
-                                .setFooter(ee.footertext, ee.footericon)
-                                .setTitle(`❌ Wyzwanie zostalo odrzucone`)
-                            );
-                        }
-                    });
-                });
+                }
             }
         }
 
