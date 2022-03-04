8a28f355459eb2c6fd397671c8ceb58127062c10
NightOwl721 <Hymm121@outlook.com>
2022-03-04 17:08:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 ﻿//Importing all needed Commands
-const { Discord, Intents } = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
+const { Client, Intents } = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
 const colors = require("colors"); //this Package is used, to change the colors of our Console! (optional and doesnt effect performance)
 const fs = require("fs"); //this package is for reading files and getting their inputs
 
 //Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
-const client = new Discord.Client({
+const client = new Client({
   messageCacheLifetime: 60,
   fetchAllMembers: false,
   messageCacheMaxSize: 10,
