3840b81600e1ddbf5434c9b83843ce317b2f0fe2
NightOwl721 <Hymm121@outlook.com>
2022-03-04 17:12:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 ﻿//Importing all needed Commands
-const { Client, Intents } = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
+const Discord = require("discord.js"); //this is the official discord.js wrapper for the Discord Api, which we use!
 const colors = require("colors"); //this Package is used, to change the colors of our Console! (optional and doesnt effect performance)
 const fs = require("fs"); //this package is for reading files and getting their inputs
 
 //Creating the Discord.js Client for This Bot with some default settings ;) and with partials, so you can fetch OLD messages
-const client = new Client({
+const client = new Discord.Client({
   messageCacheLifetime: 60,
   fetchAllMembers: false,
   messageCacheMaxSize: 10,
@@ -16,10 +16,10 @@ const client = new Client({
 });
 
 //Client variables to use everywhere
-client.commands = new client.Collection(); //an collection (like a digital map(database)) for all your commands
-client.aliases = new client.Collection(); //an collection for all your command-aliases
+client.commands = new Discord.Collection(); //an collection (like a digital map(database)) for all your commands
+client.aliases = new Discord.Collection(); //an collection for all your command-aliases
 client.categories = fs.readdirSync("./commands/"); //categories
-client.cooldowns = new client.Collection(); //an collection for cooldown commands of each user
+client.cooldowns = new Discord.Collection(); //an collection for cooldown commands of each user
 
 //Loading files, with the client variable like Command Handler, Event Handler, ...
 ["command", "events"].forEach(handler => {
